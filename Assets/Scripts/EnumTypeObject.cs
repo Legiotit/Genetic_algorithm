1c37a54d9cc1aa655be1b9e25756a8070f20f2c6
Legio <zum.q@mail.ru>
2020-08-11 20:21:28 +0500

    Add folder

---
+++ Assets/Scripts/EnumTypeObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeObject
+{
+    Vacuum = 1, 
+    Enemy = 2, 
+    Friend = 3, 
+    Food = 4, 
+    Wall = 5
+}
